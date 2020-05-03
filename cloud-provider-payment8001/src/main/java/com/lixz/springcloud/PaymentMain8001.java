c5cdfba2f30bb0ed3bbed57a3188a17a5919e0c6
lixiaozhao <lixz4@yusys.com.cn>
2020-05-03 21:10:44 +0800

    服务提供者8001端口注册服务进EurekaServer

--- cloud-provider-payment8001/src/main/java/com/lixz/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/lixz/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.lixz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lixz
  * @date 2020/4/26 - 17:02
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
