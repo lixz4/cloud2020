f8f5191b5a038a61d064d9d4667e356e1c769894
lixiaozhao <lixz4@yusys.com.cn>
2020-05-03 21:32:47 +0800

    服务消费者80端口注册服务进EurekaServer

--- cloud-consumer-order80/src/main/java/com/lixz/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/lixz/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.lixz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lixz
  * @date 2020/4/27 - 11:57
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
