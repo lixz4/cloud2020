0b3316c0e8be514428d9a318074cc6b20b501dd0
lixiaozhao <lixz4@yusys.com.cn>
2020-05-07 16:03:56 +0800

    新增8004服务，并注册到zookeeper中

---
+++ cloud-provider-payment8004/src/main/java/com/lixz/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.lixz.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lixz
+ * @date 2020/5/7 - 15:32
+ */
+@SpringBootApplication
+@EnableDiscoveryClient  //该注解用于向consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
